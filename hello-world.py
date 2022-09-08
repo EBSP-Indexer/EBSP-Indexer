f4befc13cbd9f8d1f4f94bc090c001bd82e1bcd4
Hallvard Relling <htrellin@stud.ntnu.no>
2022-09-08 15:33:15 +0200

    test

--- hello-world.py
+++ hello-world.py
@@ -1,13 +1,5 @@
 print("Hello world")
 print("Hello Hallvard")
 for i in range(3):
-    print(i)
-<<<<<<< HEAD
-
+    print(i)   
 print("Bye bye")
-
-=======
-print("Hello Hallvard")
-for i in range(3):
-    print(i)
->>>>>>> 9a27a8e378cbc036d6bafb131844e29e96db308b
