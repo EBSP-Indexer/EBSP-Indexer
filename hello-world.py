69953f9592b1b0dfbd9981888a4a37c87d3982dc
Olav <your_email@youremail.comolavlet@stud.ntnu.no>
2022-09-08 15:43:02 +0200

    init

--- hello-world.py
+++ hello-world.py
@@ -1,17 +1,15 @@
 print("Hello world")
 print("Hello Hallvard")
 for i in range(3):
-<<<<<<< HEAD
+
     print(i+1, "banan")
-=======
-    print(i)
-<<<<<<< HEAD
+
+    print(i+10)
+
 
 print("Bye bye")
 
-=======
+
 print("Hello Hallvard")
 for i in range(3):
     print(i)
->>>>>>> 9a27a8e378cbc036d6bafb131844e29e96db308b
->>>>>>> 02c35302a898aa301c3523a93e53d27fea83656e
