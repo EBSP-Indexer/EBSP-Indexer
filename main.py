ad6a033c22e186f12d4f6655a75b2a426ffcf0d7
Olav <your_email@youremail.comolavlet@stud.ntnu.no>
2022-09-15 14:06:39 +0200

    vindu

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(100,100,480,480)
+    win.setWindowTitle("Olav sitt vindu")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("Olav var her")
+    label.move(50,50)
+
+    win.show()
+    sys.exit(app.exec_())
+
 def main():
+    window()
+
     return 0
 
 main()
