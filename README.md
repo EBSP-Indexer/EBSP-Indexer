0dfee59f8ab0606d5b238bc8b7b82f8d084e6d49
Erlendos12 <99336047+Erlendos12@users.noreply.github.com>
2022-10-08 17:54:13 +0200

    Redone pattern_processing dialog ui and functions

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ Student project, work in progress, not suitable for use
 Developing an EBSD GUI based on the kikuchipy package
 
 testing git
+
+## Naming
+- Functions/ variables containing PyQt logic and pure Qt elements uses camelCase
+- Functions/ variables that contain python code and other libraries uses snake_case
+
+## Working with Qt Designer and python tutorial
+The following describes a workflow for designing GUI elements and then add Python code to the element
+
+### In .\ui\ui_QtForm
+
+### In python_object.py:
+from ui.ui_QtForm import Ui_QtForm
+
+class PythonWidget(QWidget):
+    def __init__(self, working_dir, save_path = "Pattern_avg.h5"):
+        self.ui = Ui_QtForm()
+        self.ui.setupUi(self)
--- scripts/pattern_processing.py
+++ scripts/pattern_processing.py
@@ -1,29 +1,79 @@
-import os
-import kikuchipy as kp
-from PyQt6 import uic
+from msilib.schema import Error
+from os import path
+from kikuchipy import load, filters
+from PyQt6.QtWidgets import QDialog
 
-uiForm, baseClass = uic.loadUiType("")
+from scripts.filebrowser import FileBrowser
+from ui.ui_pattern_processing_dialog import Ui_PatternProcessingWindow
 
-class background_processor(baseClass):
-    def __init__(self, working_dir, save_path = "Pattern_avg.h5"):
+
+class PatternProcessingDialog(QDialog):
+    def __init__(self, working_dir, save_name="Pattern_avg.h5"):
+        super().__init__()
         self.working_dir = working_dir
-        self.save_path = save_path
-        self.s = kp.load(os.path.join(working_dir, "Pattern.dat"), lazy=True)
-        self.ui = uiForm
+        self.save_path = f"{self.working_dir}/{save_name}"
+        self.ui = Ui_PatternProcessingWindow()
         self.ui.setupUi(self)
-        self.show()
+        self.setupConnections()
+        self.options = self.getOptions()
 
-    def remove_static(self):
-        self.s.remove_static_background()
+        try:
+            self.s = load(path.join(working_dir, "Pattern.dat"), lazy=True)
+        except:
+            print("No Pattern.dat file was found")
+            raise ValueError
+        self.gaussian_window = filters.Window("gaussian", std=1)
 
-    def remove_dynamic(self):
-        self.s.remove_dynamic_background()
+        self.fileBrowser = FileBrowser(
+            mode=FileBrowser.SaveFile,
+            dirpath=self.working_dir,
+            filter_name="Hierarchical Data Format (*.h5);;NordifUF Pattern Files (*.dat)",
+        )
 
-    def averaging(self):
-        window = kp.filters.Window("gaussian", std=1)
-        self.s.average_neighbour_patterns(window)
+    def setupConnections(self):
+        self.ui.browseButton.clicked.connect(lambda: self.setSavePath())
+        self.ui.buttonBox.accepted.connect(lambda: self.apply_processing())
+        self.ui.buttonBox.rejected.connect(lambda: self.reject())
+        self.ui.pathLineEdit.setText(self.save_path)
 
-    def save_to_file(self, *save_path):
-        if (save_path):
-            self.save_path = save_path
-        self.s.save(self.save_path)
+    def setSavePath(self):
+        if self.fileBrowser.getFile():
+            self.save_path = self.fileBrowser.getPaths()[0]
+            self.ui.pathLineEdit.setText(self.save_path)
+
+    def getOptions(self) -> dict:
+        return {
+            "static": {
+                self.ui.staticBackgroundBox.isChecked(),
+                lambda: self.s.remove_static_background(),
+            },
+            "dynamic": {
+                self.ui.dynamicBackgroundBox.isChecked(),
+                lambda: self.s.remove_dynamic_background(),
+            },
+            "average": {
+                self.ui.averageBox.isChecked(),
+                lambda: self.s.average_neighbour_patterns(self.gaussian_window),
+            },
+        }
+
+    def apply_processing(self):
+        self.options = self.getOptions()
+        for optionName, optionInfo in self.options.items():
+            optionEnabled, optionExecute = optionInfo
+            print(f"{optionName}: {optionEnabled}")
+            if optionEnabled:
+                optionExecute
+        try:
+            filepath = self.ui.pathLineEdit.text()
+            print(filepath)
+            print(path.splitext(filepath)[1])
+            self.s.save(
+                filename=filepath,
+                overwrite=True,
+                extension=path.splitext(filepath)[1][1:],
+            )
+            self.accept()
+        except Exception as e:
+            print(f"Could not save processed pattern: {e}")
+            self.reject()
