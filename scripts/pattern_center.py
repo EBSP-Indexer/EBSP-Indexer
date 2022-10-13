e27fb61ea87dffd9a56acf4b53ea0f5d3aa86462
Olav <your_email@youremail.comolavlet@stud.ntnu.no>
2022-10-13 09:39:44 +0200

    added files for pattern center

--- scripts/pattern_center.py
+++ scripts/pattern_center.py
@@ -5,6 +5,8 @@ from PySide6.QtWidgets import QDialog
 from scripts.filebrowser import FileBrowser
 from ui.ui_pattern_center import Ui_PatternCenterDialog
 
+from mplwidget import MplWidget
+
 class PatterCenterDialog(QDialog):
     def __init__(self):
         super().__init__()
@@ -15,3 +17,8 @@ class PatterCenterDialog(QDialog):
 
     def setupConnections(self):
         self.ui.buttonBox.accepted.connect(lambda: self.function)
+        self.ui.buttonTune.clicked.connect(self.plot_data)
+        
+    def plot_data(self):
+        self.plotWidget.canvas.ax.plot([0,1,2,3,4,5], [10,1,20,4,7,4])
+        self.plotWidget.canvas.draw()
