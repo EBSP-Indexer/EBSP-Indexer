a8431b5aead658a125db3f101c22ac30078edc24
Hallvard Relling <htrellin@stud.ntnu.no>
2022-10-27 16:26:30 +0200

    Cleaning up after a failed merge

--- scripts/dictionary_indexing.py
+++ scripts/dictionary_indexing.py
@@ -52,6 +52,36 @@ class DiSetupDialog(QDialog):
         # standard directory of simulated masterpattern is C:\EBSD data\kikuchipy\ebsd_simulations
         self.sim_dir = self.ui.lineEditPath.text()
 
+                # Load pattern-file to get acquisition resolution
+        try:
+            self.s = kp.load(self.pattern_path, lazy=True)
+        except Exception as e:
+            raise e
+
+        self.sig_shape = self.s.axes_manager.signal_shape[::-1]
+        bin_shapes_1D = np.array([10, 20, 30, 40, 50, 60, 12, 24, 32, 48])
+        bin_shapes_2D = np.array(
+            [
+                "(10, 10)",
+                "(20, 20)",
+                "(30, 30)",
+                "(40, 40)",
+                "(50, 50)",
+                "(60, 60)",
+                "(12, 12)",
+                "(24, 24)",
+                "(32, 32)",
+                "(48, 48)",
+            ]
+        )
+        self.bin_shapes_to_comboBox = bin_shapes_2D[
+            np.where(np.array(self.sig_shape[0] % bin_shapes_1D) == 0)[0]
+        ]
+        self.ui.comboBoxBinning.addItems(self.bin_shapes_to_comboBox)
+
+        del self.s
+        gc.collect()
+
     def setupConnections(self):
         self.ui.buttonBox.accepted.connect(lambda: self.run_dictionary_indexing())
         self.ui.buttonBox.rejected.connect(lambda: self.reject())
