4a84a1b63328a310746bd1b869c5d9df91c7d4b9
Hallvard Relling <htrellin@stud.ntnu.no>
2022-10-27 11:31:11 +0200

    Added more user input for DI

--- ui/ui_di_setup.py
+++ ui/ui_di_setup.py
@@ -16,10 +16,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QAbstractItemView, QApplication, QCheckBox,
-    QDialog, QDialogButtonBox, QDoubleSpinBox, QFrame,
-    QGridLayout, QLabel, QLayout, QLineEdit,
-    QListWidget, QListWidgetItem, QPushButton, QSizePolicy,
-    QWidget)
+    QComboBox, QDialog, QDialogButtonBox, QDoubleSpinBox,
+    QFrame, QGridLayout, QLabel, QLayout,
+    QLineEdit, QListWidget, QListWidgetItem, QPushButton,
+    QSizePolicy, QSpinBox, QWidget)
 
 class Ui_DiSetupDialog(object):
     def setupUi(self, DiSetupDialog):
@@ -41,13 +41,89 @@ class Ui_DiSetupDialog(object):
 
         self.gridLayout.addWidget(self.label_4, 4, 0, 1, 1)
 
+        self.buttonBox = QDialogButtonBox(self.centralwidget)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(False)
+
+        self.gridLayout.addWidget(self.buttonBox, 11, 4, 1, 1)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 5, 0, 1, 1)
+
+        self.checkBoxRefine = QCheckBox(self.centralwidget)
+        self.checkBoxRefine.setObjectName(u"checkBoxRefine")
+        self.checkBoxRefine.setLayoutDirection(Qt.LeftToRight)
+        self.checkBoxRefine.setChecked(True)
+        self.checkBoxRefine.setTristate(False)
+
+        self.gridLayout.addWidget(self.checkBoxRefine, 5, 2, 1, 1)
+
+        self.label_5 = QLabel(self.centralwidget)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout.addWidget(self.label_5, 10, 0, 1, 1)
+
+        self.patternCenterX = QDoubleSpinBox(self.centralwidget)
+        self.patternCenterX.setObjectName(u"patternCenterX")
+        self.patternCenterX.setDecimals(4)
+        self.patternCenterX.setMaximum(1.000000000000000)
+        self.patternCenterX.setSingleStep(0.000100000000000)
+
+        self.gridLayout.addWidget(self.patternCenterX, 2, 2, 1, 1)
+
+        self.checkBoxLazy = QCheckBox(self.centralwidget)
+        self.checkBoxLazy.setObjectName(u"checkBoxLazy")
+        self.checkBoxLazy.setChecked(True)
+
+        self.gridLayout.addWidget(self.checkBoxLazy, 6, 2, 1, 1)
+
+        self.lineEditPath = QLineEdit(self.centralwidget)
+        self.lineEditPath.setObjectName(u"lineEditPath")
+
+        self.gridLayout.addWidget(self.lineEditPath, 10, 2, 1, 1)
+
+        self.label_6 = QLabel(self.centralwidget)
+        self.label_6.setObjectName(u"label_6")
+
+        self.gridLayout.addWidget(self.label_6, 6, 0, 1, 1)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 2, 0, 1, 1)
+
+        self.patternCenterZ = QDoubleSpinBox(self.centralwidget)
+        self.patternCenterZ.setObjectName(u"patternCenterZ")
+        self.patternCenterZ.setLocale(QLocale(QLocale.English, QLocale.UnitedStates))
+        self.patternCenterZ.setDecimals(4)
+        self.patternCenterZ.setMaximum(1.000000000000000)
+        self.patternCenterZ.setSingleStep(0.000100000000000)
+
+        self.gridLayout.addWidget(self.patternCenterZ, 4, 2, 1, 1)
+
+        self.patternCenterY = QDoubleSpinBox(self.centralwidget)
+        self.patternCenterY.setObjectName(u"patternCenterY")
+        self.patternCenterY.setDecimals(4)
+        self.patternCenterY.setMaximum(1.000000000000000)
+        self.patternCenterY.setSingleStep(0.000100000000000)
+
+        self.gridLayout.addWidget(self.patternCenterY, 3, 2, 1, 1)
+
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
+
         self.pathLabel = QLabel(self.centralwidget)
         self.pathLabel.setObjectName(u"pathLabel")
         self.pathLabel.setLayoutDirection(Qt.LeftToRight)
         self.pathLabel.setScaledContents(False)
         self.pathLabel.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
 
-        self.gridLayout.addWidget(self.pathLabel, 7, 0, 1, 1)
+        self.gridLayout.addWidget(self.pathLabel, 9, 0, 1, 1)
 
         self.listWidgetPhase = QListWidget(self.centralwidget)
         QListWidgetItem(self.listWidgetPhase)
@@ -60,85 +136,53 @@ class Ui_DiSetupDialog(object):
         self.listWidgetPhase.setMinimumSize(QSize(0, 50))
         self.listWidgetPhase.setSelectionMode(QAbstractItemView.MultiSelection)
 
-        self.gridLayout.addWidget(self.listWidgetPhase, 7, 1, 1, 1)
+        self.gridLayout.addWidget(self.listWidgetPhase, 9, 2, 1, 1)
 
-        self.buttonBox = QDialogButtonBox(self.centralwidget)
-        self.buttonBox.setObjectName(u"buttonBox")
-        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.label_7 = QLabel(self.centralwidget)
+        self.label_7.setObjectName(u"label_7")
 
-        self.gridLayout.addWidget(self.buttonBox, 9, 2, 1, 1)
+        self.gridLayout.addWidget(self.label_7, 2, 3, 1, 1)
 
-        self.label_3 = QLabel(self.centralwidget)
-        self.label_3.setObjectName(u"label_3")
+        self.label_8 = QLabel(self.centralwidget)
+        self.label_8.setObjectName(u"label_8")
 
-        self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
-
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout.addWidget(self.label, 5, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_8, 3, 3, 1, 1)
 
         self.pushButtonBrowse = QPushButton(self.centralwidget)
         self.pushButtonBrowse.setObjectName(u"pushButtonBrowse")
 
-        self.gridLayout.addWidget(self.pushButtonBrowse, 8, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButtonBrowse, 10, 4, 1, 1)
 
-        self.checkBoxRefine = QCheckBox(self.centralwidget)
-        self.checkBoxRefine.setObjectName(u"checkBoxRefine")
-        self.checkBoxRefine.setChecked(True)
+        self.label_9 = QLabel(self.centralwidget)
+        self.label_9.setObjectName(u"label_9")
 
-        self.gridLayout.addWidget(self.checkBoxRefine, 5, 1, 1, 1)
+        self.gridLayout.addWidget(self.label_9, 7, 0, 1, 1)
 
-        self.label_5 = QLabel(self.centralwidget)
-        self.label_5.setObjectName(u"label_5")
+        self.comboBoxBinning = QComboBox(self.centralwidget)
+        self.comboBoxBinning.setObjectName(u"comboBoxBinning")
 
-        self.gridLayout.addWidget(self.label_5, 8, 0, 1, 1)
+        self.gridLayout.addWidget(self.comboBoxBinning, 2, 4, 1, 1)
 
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
+        self.doubleSpinBoxStepSize = QDoubleSpinBox(self.centralwidget)
+        self.doubleSpinBoxStepSize.setObjectName(u"doubleSpinBoxStepSize")
 
-        self.gridLayout.addWidget(self.label_2, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.doubleSpinBoxStepSize, 3, 4, 1, 1)
 
-        self.lineEditPath = QLineEdit(self.centralwidget)
-        self.lineEditPath.setObjectName(u"lineEditPath")
+        self.checkBoxMask = QCheckBox(self.centralwidget)
+        self.checkBoxMask.setObjectName(u"checkBoxMask")
 
-        self.gridLayout.addWidget(self.lineEditPath, 8, 1, 1, 1)
+        self.gridLayout.addWidget(self.checkBoxMask, 7, 2, 1, 1)
 
-        self.checkBoxLazy = QCheckBox(self.centralwidget)
-        self.checkBoxLazy.setObjectName(u"checkBoxLazy")
-        self.checkBoxLazy.setChecked(True)
+        self.label_10 = QLabel(self.centralwidget)
+        self.label_10.setObjectName(u"label_10")
 
-        self.gridLayout.addWidget(self.checkBoxLazy, 6, 1, 1, 1)
+        self.gridLayout.addWidget(self.label_10, 4, 3, 1, 1)
 
-        self.label_6 = QLabel(self.centralwidget)
-        self.label_6.setObjectName(u"label_6")
+        self.spinBoxNumIter = QSpinBox(self.centralwidget)
+        self.spinBoxNumIter.setObjectName(u"spinBoxNumIter")
+        self.spinBoxNumIter.setMaximum(99)
 
-        self.gridLayout.addWidget(self.label_6, 6, 0, 1, 1)
-
-        self.patternCenterX = QDoubleSpinBox(self.centralwidget)
-        self.patternCenterX.setObjectName(u"patternCenterX")
-        self.patternCenterX.setDecimals(4)
-        self.patternCenterX.setMaximum(1.000000000000000)
-        self.patternCenterX.setSingleStep(0.000100000000000)
-
-        self.gridLayout.addWidget(self.patternCenterX, 2, 1, 1, 1)
-
-        self.patternCenterY = QDoubleSpinBox(self.centralwidget)
-        self.patternCenterY.setObjectName(u"patternCenterY")
-        self.patternCenterY.setDecimals(4)
-        self.patternCenterY.setMaximum(1.000000000000000)
-        self.patternCenterY.setSingleStep(0.000100000000000)
-
-        self.gridLayout.addWidget(self.patternCenterY, 3, 1, 1, 1)
-
-        self.patternCenterZ = QDoubleSpinBox(self.centralwidget)
-        self.patternCenterZ.setObjectName(u"patternCenterZ")
-        self.patternCenterZ.setLocale(QLocale(QLocale.English, QLocale.UnitedStates))
-        self.patternCenterZ.setDecimals(4)
-        self.patternCenterZ.setMaximum(1.000000000000000)
-        self.patternCenterZ.setSingleStep(0.000100000000000)
-
-        self.gridLayout.addWidget(self.patternCenterZ, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.spinBoxNumIter, 4, 4, 1, 1)
 
 
         self.gridLayout_2.addWidget(self.centralwidget, 0, 0, 1, 1)
@@ -146,7 +190,6 @@ class Ui_DiSetupDialog(object):
         QWidget.setTabOrder(self.checkBoxRefine, self.checkBoxLazy)
         QWidget.setTabOrder(self.checkBoxLazy, self.listWidgetPhase)
         QWidget.setTabOrder(self.listWidgetPhase, self.lineEditPath)
-        QWidget.setTabOrder(self.lineEditPath, self.pushButtonBrowse)
 
         self.retranslateUi(DiSetupDialog)
 
@@ -156,6 +199,14 @@ class Ui_DiSetupDialog(object):
     def retranslateUi(self, DiSetupDialog):
         DiSetupDialog.setWindowTitle(QCoreApplication.translate("DiSetupDialog", u"Pattern Processing", None))
         self.label_4.setText(QCoreApplication.translate("DiSetupDialog", u"PC - z", None))
+        self.label.setText(QCoreApplication.translate("DiSetupDialog", u"Refine", None))
+        self.checkBoxRefine.setText("")
+        self.label_5.setText(QCoreApplication.translate("DiSetupDialog", u"Master patterns", None))
+        self.checkBoxLazy.setText("")
+        self.lineEditPath.setText(QCoreApplication.translate("DiSetupDialog", u"C:\\EBSD data\\kikuchipy\\ebsd_simulations", None))
+        self.label_6.setText(QCoreApplication.translate("DiSetupDialog", u"Lazy", None))
+        self.label_2.setText(QCoreApplication.translate("DiSetupDialog", u"PC - x", None))
+        self.label_3.setText(QCoreApplication.translate("DiSetupDialog", u"PC - y", None))
         self.pathLabel.setText(QCoreApplication.translate("DiSetupDialog", u"Phase", None))
 
         __sortingEnabled = self.listWidgetPhase.isSortingEnabled()
@@ -174,14 +225,11 @@ class Ui_DiSetupDialog(object):
         ___qlistwidgetitem5.setText(QCoreApplication.translate("DiSetupDialog", u"austenite", None));
         self.listWidgetPhase.setSortingEnabled(__sortingEnabled)
 
-        self.label_3.setText(QCoreApplication.translate("DiSetupDialog", u"PC - y", None))
-        self.label.setText(QCoreApplication.translate("DiSetupDialog", u"Refine", None))
+        self.label_7.setText(QCoreApplication.translate("DiSetupDialog", u"Binning shape", None))
+        self.label_8.setText(QCoreApplication.translate("DiSetupDialog", u"Step-size (\u00b0)", None))
         self.pushButtonBrowse.setText(QCoreApplication.translate("DiSetupDialog", u"Browse", None))
-        self.checkBoxRefine.setText("")
-        self.label_5.setText(QCoreApplication.translate("DiSetupDialog", u"Master patterns", None))
-        self.label_2.setText(QCoreApplication.translate("DiSetupDialog", u"PC - x", None))
-        self.lineEditPath.setText(QCoreApplication.translate("DiSetupDialog", u"C:\\EBSD data\\kikuchipy\\ebsd_simulations", None))
-        self.checkBoxLazy.setText("")
-        self.label_6.setText(QCoreApplication.translate("DiSetupDialog", u"Lazy", None))
+        self.label_9.setText(QCoreApplication.translate("DiSetupDialog", u"Use circular mask", None))
+        self.checkBoxMask.setText("")
+        self.label_10.setText(QCoreApplication.translate("DiSetupDialog", u"Matching per iteration", None))
     # retranslateUi
 
