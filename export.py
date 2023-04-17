72b1e1d6b172b9850ec47b60519bab35eac47486
Erlendos12 <99336047+Erlendos12@users.noreply.github.com>
2023-04-17 10:56:12 +0200

    Allows users to specify target architecture

--- export.py
+++ export.py
@@ -1,11 +1,17 @@
 import os
-import PyInstaller.__main__
 import platform
 import sys
 
-sys.setrecursionlimit(sys.getrecursionlimit() * 5)
+import PyInstaller.__main__
+
+sys.setrecursionlimit(sys.getrecursionlimit() * 5) # Fix needed for some systems
 workdir = os.getcwd()
 
+target_arch = input(
+    "Specify the target arcitecture [auto, x86_64, arm64, universal2]: "
+    ).lower().strip()
+
+# Different seperators between macOS and Windows
 if platform.system().lower() == "darwin":
     sep = ":"
 else:
@@ -14,23 +20,25 @@ else:
 args_list = [
     '--noconfirm',
     '--onedir',
-    #'--uac-admin',
     '--icon', os.path.join(workdir, "resources/ebsd_gui.ico"),
     '--name', "EBSD-GUI",
     '--add-data', os.path.join(workdir, f"resources{sep}resources/"),
-    '--add-data', os.path.join(workdir, f"*G.txt{sep}."),
+    '--add-data', os.path.join(workdir, f"*G.txt{sep}."),   # Add COPYING.txt
     '--additional-hooks-dir', os.path.join(workdir,"hooks"),
     '--hidden-import', "kikuchipy",
     '--hidden-import', "hyperspy",
     '--hidden-import', "pyebsdindex",
 ]
+# Checks target architecture for valid values
+if target_arch in ['x86_64', 'arm64', 'universal2']:
+    args_list.append("--target-arch") 
+    args_list.append(target_arch) 
 
+# Skips creating application on macOS as it currently do not work
 if platform.system().lower() == "darwin":
     args_list.append("--console")
-    #args_list.append("--target-arch") 
-    #args_list.append("x86_64") # Valid values are x86_64, arm64, and universal2.
 else:
     args_list.append("--windowed")
 
 args_list.append('main.py')
-PyInstaller.__main__.run(args_list)
+PyInstaller.__main__.run(args_list) # Runs the command with arguments
