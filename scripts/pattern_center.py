131e8728cc202e696e0f58fcb42c43738b436bec
Erlendos12 <99336047+Erlendos12@users.noreply.github.com>
2023-04-26 15:24:07 +0200

    Black format and added warning for no setting.txt

--- scripts/pattern_center.py
+++ scripts/pattern_center.py
@@ -17,14 +17,15 @@ progressbar_bool = False
 FCC = ["ni", "al", "austenite", "cu", "si", "ag", "cu"]
 BCC = ["ferrite"]
 
+
 def find_hkl(phase):
-    #TETRAGONAL = ["steel_sigma"]
+    # TETRAGONAL = ["steel_sigma"]
     if phase.lower() in FCC:
         return [[1, 1, 1], [2, 0, 0], [2, 2, 0], [3, 1, 1]]
     elif phase.lower() in BCC:
         return [[0, 1, 1], [0, 0, 2], [1, 1, 2], [0, 2, 2]]
-    #experimental support for TETRAGONAL sigma phase, not sure if correct...
-    #elif phase.lower() in TETRAGONAL:
+    # experimental support for TETRAGONAL sigma phase, not sure if correct...
+    # elif phase.lower() in TETRAGONAL:
     #    return [[1, 1, 0], [2, 0, 0], [1, 0, 1], [2, 1, 0], [1, 1, 1], [2, 2, 0], [2, 1, 1]]
 
 
@@ -32,72 +33,57 @@ class PatterCenterDialog(QDialog):
     def __init__(self, parent=None, file_selected=None):
         super().__init__(parent)
         self.file_selected = file_selected
-        self.setting_path = path.join(
-            get_setting_file_bottom_top(
-                file_selected, 
-                'Setting.txt', 
-                return_dir_path=True
-            )[1], 
-            "Setting.txt"
+        _, setting_dir_path = get_setting_file_bottom_top(
+            file_selected, "Setting.txt", return_dir_path=True
         )
+        if setting_dir_path is None:
+            raise Exception("No Setting.txt with calibration patterns were found.")
+        else:
+            self.setting_path = path.join(
+                setting_dir_path,
+                "Setting.txt",
+            )
         self.ui = Ui_PatternCenter()
         self.ui.setupUi(self)
         self.setupConnections()
         self.setupCalibrationPatterns()
         self.setupInitialSettings()
-        
-        #self.fileBrowserOD = FileBrowser(FileBrowser.OpenDirectory)
-        self.fileBrowserOD = FileBrowser(
-            mode=FileBrowser.OpenFile,
-            filter_name="*.h5"
-        )
 
-    # Should not be needed anymore
-    # def findSettingsFile(self):
-    #     if self.file_selected[-11:] == "Setting.txt":
-    #         setting_path = self.file_selected
-    #         return setting_path
-
-    #     if path.exists(path.join(self.file_selected, "Setting.txt")):
-    #         setting_path = path.join(self.file_selected, "Setting.txt")
-    #         return setting_path
-
-    #     if path.exists(path.join(path.dirname(self.file_selected), "Setting.txt")):
-    #         setting_path = path.join(path.dirname(self.file_selected), "Setting.txt")
-    #         return setting_path
+        # self.fileBrowserOD = FileBrowser(FileBrowser.OpenDirectory)
+        self.fileBrowserOF = FileBrowser(mode=FileBrowser.OpenFile, filter_name="*.h5")
 
     def setupInitialSettings(self):
-        self.setting_file = SettingFile(path.join(path.dirname(self.setting_path),"project_settings.txt"))
+        self.setting_file = SettingFile(
+            path.join(path.dirname(self.setting_path), "project_settings.txt")
+        )
         self.program_settings = SettingFile("advanced_settings.txt")
-        
+
         try:
             self.convention = self.setting_file.read("Convention")
         except:
             self.convention = self.program_settings.read("Convention")
-        
+
         try:
             self.pc = np.array(eval(self.setting_file.read("PC")))
-            # self.pc = [
-            #         float(self.setting_file.read("X star")),
-            #         float(self.setting_file.read("Y star")),
-            #         float(self.setting_file.read("Z star")),
-            # ]
         except:
-            if self.s_cal.metadata.Acquisition_instrument.SEM.microscope == "ZEISS SUPRA55 VP":
+            if (
+                self.s_cal.metadata.Acquisition_instrument.SEM.microscope
+                == "ZEISS SUPRA55 VP"
+            ):
                 self.pc = [
-                    0.5605-0.0017*float(self.working_distance),
-                    1.2056-0.0225*float(self.working_distance),
+                    0.5605 - 0.0017 * float(self.working_distance),
+                    1.2056 - 0.0225 * float(self.working_distance),
                     0.483,
                 ]
-            else:    
+            else:
                 self.pc = np.array([0.5000, 0.5000, 0.5000])
-        
+
         if self.convention == "TSL":
-            #Store TSL convention in BRUKER convention
+            # Store TSL convention in BRUKER convention
             self.pc[1] = 1 - self.pc[1]
             self.ui.conventionBox.setCurrentIndex(1)
         self.updatePCSpinBox()
-        
+
         self.mp_paths = {}
 
         i = 1
@@ -114,7 +100,7 @@ class PatterCenterDialog(QDialog):
                 self.mp_paths[mp.phase.name] = mp_path
                 self.ui.listPhases.addItem(mp.phase.name)
                 if mp.phase.name not in FCC + BCC:
-                    self.ui.listPhases.item(i-1).setFlags(Qt.NoItemFlags)
+                    self.ui.listPhases.item(i - 1).setFlags(Qt.NoItemFlags)
                 i += 1
             except:
                 break
@@ -129,10 +115,10 @@ class PatterCenterDialog(QDialog):
         self.enabled = False
         self.ui.bandButton.setDisabled(True)
         self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
-    
+
     def changeStateOfButtons(self):
         enable = bool(len(list(self.mp_paths.keys())))
-        
+
         self.ui.buttonPlot.setEnabled(enable)
         self.ui.buttonTune.setEnabled(enable)
         self.ui.toolButtonLeft.setEnabled(enable)
@@ -148,19 +134,26 @@ class PatterCenterDialog(QDialog):
         self.s_cal.remove_static_background(show_progressbar=progressbar_bool)
         self.s_cal.remove_dynamic_background(show_progressbar=progressbar_bool)
 
-        self.working_distance = self.s_cal.metadata.Acquisition_instrument.SEM.working_distance
-        self.ui.workingDistanceLabel.setText("Working Distance (mm): "+str(self.working_distance))
+        self.working_distance = (
+            self.s_cal.metadata.Acquisition_instrument.SEM.working_distance
+        )
+        self.ui.workingDistanceLabel.setText(
+            "Working Distance (mm): " + str(self.working_distance)
+        )
 
         self.indexer = ebsd_index.EBSDIndexer(
             phaselist=["FCC", "BCC"],
-            vendor="BRUKER", 
+            vendor="BRUKER",
             sampleTilt=self.s_cal.detector.sample_tilt,
             camElev=self.s_cal.detector.tilt,
             patDim=sig_shape,
         )
 
         self.ui.counterLabel.setText(
-            "Calibration Pattern: "+str(self.pattern_index+1)+"/"+str(self.nav_size)
+            "Calibration Pattern: "
+            + str(self.pattern_index + 1)
+            + "/"
+            + str(self.nav_size)
         )
         self.ui.MplWidget.canvas.ax.axis("off")
         self.ui.MplWidget.canvas.ax.imshow(self.s_cal.data[0], cmap="gray")
@@ -181,58 +174,68 @@ class PatterCenterDialog(QDialog):
         self.ui.buttonBox.rejected.connect(lambda: self.reject())
         self.ui.bandButton.clicked.connect(lambda: self.bandButtonClicked())
         self.ui.ignoreCheckBox.clicked.connect(lambda: self.ignoreButtonClicked())
-        self.ui.conventionBox.currentTextChanged.connect(lambda: self.updatePCConvention())
+        self.ui.conventionBox.currentTextChanged.connect(
+            lambda: self.updatePCConvention()
+        )
 
     def previousPattern(self):
         if self.pattern_index > 0:
-            #Saves current phase in pcs dictionary
+            # Saves current phase in pcs dictionary
             self.pcs[self.pattern_index][0] = self.phase
             self.pcs[self.pattern_index][1] = self.pc
             self.pcs[self.pattern_index][2] = self.pattern_ignored
 
-            #Gets values for previous phase
+            # Gets values for previous phase
             self.pattern_index -= 1
             self.phase = self.pcs.get(self.pattern_index)[0]
             self.pc = self.pcs.get(self.pattern_index)[1]
             self.pattern_ignored = self.pcs.get(self.pattern_index)[2]
             self.ui.counterLabel.setText(
-                "Calibration Pattern: "+str(self.pattern_index+1)+"/"+str(self.nav_size)
-                )
+                "Calibration Pattern: "
+                + str(self.pattern_index + 1)
+                + "/"
+                + str(self.nav_size)
+            )
             self.ui.ignoreCheckBox.setChecked(self.pattern_ignored)
             self.updatePCSpinBox()
             self.plotData()
 
     def nextPattern(self):
-        if self.pattern_index < self.nav_size-1:
+        if self.pattern_index < self.nav_size - 1:
             self.pcs[self.pattern_index][0] = self.phase
             self.pcs[self.pattern_index][1] = self.pc
             self.pcs[self.pattern_index][2] = self.pattern_ignored
 
             self.pattern_index += 1
             if self.pcs.get(self.pattern_index)[1][0] == 0.0:
-                self.pc = self.pcs.get(self.pattern_index-1)[1]
+                self.pc = self.pcs.get(self.pattern_index - 1)[1]
             else:
                 self.pc = self.pcs.get(self.pattern_index)[1]
             if self.pcs.get(self.pattern_index)[0] == "":
-                self.phase = self.pcs.get(self.pattern_index-1)[0]
+                self.phase = self.pcs.get(self.pattern_index - 1)[0]
             else:
                 self.phase = self.pcs.get(self.pattern_index)[0]
             self.pattern_ignored = self.pcs.get(self.pattern_index)[2]
 
             self.ui.counterLabel.setText(
-                "Calibration Pattern: "+str(self.pattern_index+1)+"/"+str(self.nav_size)
-                )
+                "Calibration Pattern: "
+                + str(self.pattern_index + 1)
+                + "/"
+                + str(self.nav_size)
+            )
             self.ui.ignoreCheckBox.setChecked(self.pattern_ignored)
             self.updatePCSpinBox()
             self.plotData()
 
     def addPhase(self):
-        if self.fileBrowserOD.getFile():
-            mp_path = self.fileBrowserOD.getPaths()[0]
+        if self.fileBrowserOF.getFile():
+            mp_path = self.fileBrowserOF.getPaths()[0]
             try:
                 mp = kp.load(mp_path, lazy=True)
                 if not len(mp.phase.name):
-                    mp.phase.name = path.basename(path.dirname(mp_path)) #.split("/").pop()
+                    mp.phase.name = path.basename(
+                        path.dirname(mp_path)
+                    )  # .split("/").pop()
                 phase_name = mp.phase.name
             except Exception as e:
                 raise e
@@ -240,17 +243,19 @@ class PatterCenterDialog(QDialog):
                 self.mp_paths[phase_name] = mp_path
                 self.ui.listPhases.addItem(phase_name)
                 self.is_mp_paths_updated = True
-            
-            self.fileBrowserOD.setDefaultDir(path.dirname(mp_path))
+
+            self.fileBrowserOF.setDefaultDir(path.dirname(mp_path))
             if phase_name not in FCC + BCC:
-                self.ui.listPhases.item(len(self.mp_paths.keys())-1).setFlags(Qt.NoItemFlags)
+                self.ui.listPhases.item(len(self.mp_paths.keys()) - 1).setFlags(
+                    Qt.NoItemFlags
+                )
             else:
                 self.phase = phase_name
-                self.ui.listPhases.setCurrentRow(len(self.mp_paths.keys())-1)
+                self.ui.listPhases.setCurrentRow(len(self.mp_paths.keys()) - 1)
         self.changeStateOfButtons()
 
     def removePhase(self):
-        #if str(self.ui.listPhases.currentItem().text()) is None:
+        # if str(self.ui.listPhases.currentItem().text()) is None:
         #    self.ui.listPhases.setCurrentRow(-1)
         self.mp_paths.pop(str(self.ui.listPhases.currentItem().text()))
         self.ui.listPhases.takeItem(self.ui.listPhases.currentRow())
@@ -265,7 +270,7 @@ class PatterCenterDialog(QDialog):
         if self.convention == "BRUKER":
             self.ui.spinBoxY.setValue(self.pc[1])
         elif self.convention == "TSL":
-            self.ui.spinBoxY.setValue(1-self.pc[1])
+            self.ui.spinBoxY.setValue(1 - self.pc[1])
 
     def updatePCArrayFromSpinBox(self):
         self.pc[0] = self.ui.spinBoxX.value()
@@ -274,30 +279,30 @@ class PatterCenterDialog(QDialog):
             self.pc[1] = self.ui.spinBoxY.value()
         elif self.convention == "TSL":
             self.pc[1] = 1 - self.ui.spinBoxY.value()
-    
+
     def updatePCConvention(self):
         self.convention = self.ui.conventionBox.currentText()
-        self.updatePCSpinBox()      
+        self.updatePCSpinBox()
 
     def updatePCDict(self, pattern_index, phase, pc, pattern_ignored):
         self.pcs[pattern_index] = [phase, pc, pattern_ignored]
-    
+
     def plotClicked(self):
         self.updatePCArrayFromSpinBox()
 
-        #Checks which phase to use from list of phases
+        # Checks which phase to use from list of phases
         try:
             self.phase = self.ui.listPhases.currentItem().text()
         except:
             self.ui.listPhases.setCurrentRow(0)
             self.phase = self.ui.listPhases.currentItem().text()
-        
+
         self.plotData()
 
     def refinePatternCenter(self):
         self.updatePCArrayFromSpinBox()
 
-        #Checks which phase to use from list of phases
+        # Checks which phase to use from list of phases
         try:
             self.phase = self.ui.listPhases.currentItem().text()
         except:
@@ -337,18 +342,18 @@ class PatterCenterDialog(QDialog):
             self.simulator_dict[name] = kp.simulations.KikuchiPatternSimulator(
                 ref_dict[name]
             )
-        
+
         # To prevent the program from retrieving mp data when not needed:
         self.is_mp_paths_updated = False
 
     def plotData(self):
         self.updatePCDict(self.pattern_index, self.phase, self.pc, self.pattern_ignored)
 
-        #Updates data from master pattern (simulator_dict) if phase list has been updated
+        # Updates data from master pattern (simulator_dict) if phase list has been updated
         if self.is_mp_paths_updated:
             self.retrieveMPData()
-        
-        #Sets up data needed for plotting
+
+        # Sets up data needed for plotting
         detector = kp.detectors.EBSDDetector(
             shape=self.s_cal.detector.shape,
             sample_tilt=self.s_cal.detector.sample_tilt,
@@ -357,12 +362,16 @@ class PatterCenterDialog(QDialog):
             convention=self.indexer.vendor,
         )
 
-        data = self.indexer.index_pats(self.s_cal.data[self.pattern_index], PC=self.pc)[0]
-        rot = Rotation(data["quat"][-1]) * Rotation.from_axes_angles([0, 0, -1], np.pi / 2)
+        data = self.indexer.index_pats(self.s_cal.data[self.pattern_index], PC=self.pc)[
+            0
+        ]
+        rot = Rotation(data["quat"][-1]) * Rotation.from_axes_angles(
+            [0, 0, -1], np.pi / 2
+        )
 
         geosim_dict = {}
 
-        sys.stdout = open(devnull, 'w')
+        sys.stdout = open(devnull, "w")
         for name in self.mp_paths.keys():
             if name not in FCC + BCC:
                 continue
@@ -370,26 +379,32 @@ class PatterCenterDialog(QDialog):
 
         sys.stdout = sys.__stdout__
 
-
-        #Draws pattern in MplWidget 
+        # Draws pattern in MplWidget
         self.pattern_image = self.s_cal.data[self.pattern_index]
         self.ui.MplWidget.canvas.ax.clear()
         self.ui.MplWidget.canvas.ax.axis("off")
         self.ui.MplWidget.canvas.ax.imshow(self.pattern_image, cmap="gray")
-        self.lines, zone_axes, zone_axes_labels = geosim_dict[self.phase].as_collections(
+        self.lines, zone_axes, zone_axes_labels = geosim_dict[
+            self.phase
+        ].as_collections(
             0,
             zone_axes=True,
             zone_axes_labels=True,
             zone_axes_labels_kwargs=dict(fontsize=12),
         )
 
-        pc_x, pc_y = len(self.pattern_image)*self.pc[0], len(self.pattern_image[0])*self.pc[1]
-        self.ui.MplWidget.canvas.ax.plot(pc_x, pc_y, marker="*", markersize=12, color="gold")
+        pc_x, pc_y = (
+            len(self.pattern_image) * self.pc[0],
+            len(self.pattern_image[0]) * self.pc[1],
+        )
+        self.ui.MplWidget.canvas.ax.plot(
+            pc_x, pc_y, marker="*", markersize=12, color="gold"
+        )
         self.ui.MplWidget.canvas.ax.add_collection(self.lines)
         self.ui.MplWidget.canvas.draw()
 
-        #Updates misfit label
-        self.ui.labelMisfit.setText("Misfit(°): "+str(round(data["fit"][-1][0], 4)))
+        # Updates misfit label
+        self.ui.labelMisfit.setText("Misfit(°): " + str(round(data["fit"][-1][0], 4)))
 
         self.bands_enabled = False
         self.bandButtonClicked()
@@ -407,12 +422,17 @@ class PatterCenterDialog(QDialog):
             self.ui.MplWidget.canvas.ax.clear()
             self.ui.MplWidget.canvas.ax.axis("off")
             self.ui.MplWidget.canvas.ax.imshow(self.pattern_image, cmap="gray")
-            pc_x, pc_y = len(self.pattern_image)*self.pc[0], len(self.pattern_image[0])*self.pc[1]
-            self.ui.MplWidget.canvas.ax.plot(pc_x, pc_y, marker="*", markersize=12, color="gold")
+            pc_x, pc_y = (
+                len(self.pattern_image) * self.pc[0],
+                len(self.pattern_image[0]) * self.pc[1],
+            )
+            self.ui.MplWidget.canvas.ax.plot(
+                pc_x, pc_y, marker="*", markersize=12, color="gold"
+            )
             self.ui.MplWidget.canvas.draw()
             self.ui.bandButton.setText("Show bands")
             self.bands_enabled = False
-    
+
     def ignoreButtonClicked(self):
         if self.ui.ignoreCheckBox.isChecked():
             self.pattern_ignored = True
@@ -431,25 +451,30 @@ class PatterCenterDialog(QDialog):
         for i in range(self.nav_size):
             pattern_included = not self.pcs.get(i)[2]
             if self.pcs.get(i)[1][0] > 0 and pattern_included:
-                x, y, z = self.pcs.get(i)[1][0], self.pcs.get(i)[1][1], self.pcs.get(i)[1][2]
+                x, y, z = (
+                    self.pcs.get(i)[1][0],
+                    self.pcs.get(i)[1][1],
+                    self.pcs.get(i)[1][2],
+                )
                 x_sum += x
                 y_sum += y
                 z_sum += z
                 n += 1
                 if self.program_settings.read("Individual PC data") == "True":
-                    self.setting_file.write("Calibration PC"+str(i), str([round(x, 4),round(y, 4), round(z, 4)]))
+                    self.setting_file.write(
+                        "Calibration PC" + str(i),
+                        str([round(x, 4), round(y, 4), round(z, 4)]),
+                    )
 
-        x_average = round(x_sum/n, 4)
-        y_average = round(y_sum/n, 4)
-        z_average = round(z_sum/n, 4)
+        x_average = round(x_sum / n, 4)
+        y_average = round(y_sum / n, 4)
+        z_average = round(z_sum / n, 4)
 
-        self.setting_file.write("Convention", self.convention)       
-        # self.setting_file.write("X star", f"{x_average:.4}")
+        self.setting_file.write("Convention", self.convention)
         if self.convention == "BRUKER":
             self.setting_file.write("PC", (x_average, y_average, z_average))
         elif self.convention == "TSL":
-            self.setting_file.write("PC", (x_average, 1-y_average, z_average))
-        # self.setting_file.write("Z star", f"{z_average:.4}")
-        
+            self.setting_file.write("PC", (x_average, 1 - y_average, z_average))
+
         self.setting_file.save()
         self.close()
